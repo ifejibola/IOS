fc2a58b22c73059f27c0db32d1d3eb38258914f7
Ife <ife.jibola@gmail.com>
2019-01-11 12:03:32 -0500

    button with code

---
+++ CreateButtonWCode/CreateButtonWCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CreateButtonWCode
+//
+//  Created by Ife Ajibola on 2019-01-10.
+//  Copyright © 2019 Ife Ajibola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
